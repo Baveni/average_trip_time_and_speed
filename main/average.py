42bf6ea7ba1a09e4bead0939aa9440da011275f9
jernej <uranic.jernejvgmail.com>
2018-04-29 12:08:23 +0200

    just some sunday afternoon moneky biz

---
+++ main/average.py
@@ -0,0 +1,30 @@
+print("This here will calculate your average trip time \n"
+      "and your average trip speed")
+
+# feed me some info please :)
+
+trip_distance = int(input("enter trip distance>>"))
+start_hour = int(input("enter starting hour>>"))
+start_minutes = int(input("enter starting minutes>>"))
+stop_hour = int(input("enter ending hour>>"))
+stop_minutes = int(input("enter ending minutes>>"))
+# trip_distance = 30
+# start_hour = 16
+# start_minutes = 35
+# stop_hour = 18
+# stop_minutes = 5
+
+# calculate time in hours
+diff_h = stop_hour - start_hour
+diff_m = stop_minutes - start_minutes
+
+time_in_minutes = 60 * diff_h + diff_m
+time_in_hours = time_in_minutes / 60
+
+
+# calculate average speed in km/h
+avg_speed = trip_distance / time_in_hours
+
+# print time and speed
+print("You spend ", time_in_hours, " hours for the trip")
+print("Youre avg speed was ", avg_speed, "km/h")
