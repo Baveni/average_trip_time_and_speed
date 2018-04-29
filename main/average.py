06ace4b170de29558d7958227589ddd4e340e21c
jernej <uranic.jernejvgmail.com>
2018-04-29 15:47:59 +0200

    adeed round to two decimal

--- main/average.py
+++ main/average.py
@@ -4,10 +4,43 @@ print("This here will calculate your average trip time \n"
 # feed me some info please :)
 
 trip_distance = int(input("enter trip distance>>"))
-start_hour = int(input("enter starting hour>>"))
-start_minutes = int(input("enter starting minutes>>"))
-stop_hour = int(input("enter ending hour>>"))
-stop_minutes = int(input("enter ending minutes>>"))
+
+while True:
+
+    start_hour = int(input("enter starting hour>>"))
+
+    if start_hour > 23:
+        print("please input hour between 0 and 23")
+        start_hour = int(input("enter starting hour>>"))
+    else:
+        pass
+
+    start_minutes = int(input("enter starting minutes>>"))
+
+    if start_minutes > 59:
+        print("please input minutes between 0 and 59")
+        start_minutes = int(input("enter starting minutes>>"))
+    else:
+        pass
+
+
+    stop_hour = int(input("enter ending hour>>"))
+
+    if stop_hour > 23:
+        print("please input hour between 0 and 23")
+        stop_hour = int(input("enter ending hour>>"))
+    else:
+        pass
+
+    stop_minutes = int(input("enter ending minutes>>"))
+
+    if stop_minutes <= 59:
+        break
+    elif stop_minutes > 59:
+        print("please input minutes between 0 and 59")
+        stop_minutes = int(input("enter ending minutes>>"))
+
+
 
 
 # calculate time in hours
@@ -22,5 +55,7 @@ time_in_hours = time_in_minutes / 60
 avg_speed = trip_distance / time_in_hours
 
 # print time and speed
-print("You spend ", time_in_hours, " hours for the trip")
-print("Your avg speed was ", avg_speed, "km/h")
+print("You spend", round(time_in_hours, 2), "hours for the trip")
+print("Your avg speed was", round(avg_speed, 2), "km/h")
+
+# it's not perfect, yet, maybe.. in future
